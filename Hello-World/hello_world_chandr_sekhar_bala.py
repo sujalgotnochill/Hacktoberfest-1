aa55f07eb2910223850f53ddec44ab28dedddeea
Chandra Sekhar Bala <57448981+Chandra-Sekhar-Bala@users.noreply.github.com>
2021-10-01 20:28:18 +0530

    Update hello_world_chandr_sekhar_bala.py

--- Hello-World/hello_world_chandr_sekhar_bala.py
+++ Hello-World/hello_world_chandr_sekhar_bala.py
@@ -1,4 +1,6 @@
-/ LANGUAGE: Python
-// AUTHOR: Chandra Sekhar Bala
-// GITHUB: https://github.com/Chandra-Sekhar-Bala
+'''
+LANGUAGE: Python
+AUTHOR: Chandra Sekhar Bala
+GITHUB: https://github.com/Chandra-Sekhar-Bala
+'''
 print('Hello, World!')
