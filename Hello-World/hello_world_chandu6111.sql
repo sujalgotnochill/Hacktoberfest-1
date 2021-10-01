b776b639e8a788f3410b60384e27efbf3f8767a2
Damarla Venkata Sai Chandana <66585358+chandu6111@users.noreply.github.com>
2021-10-01 22:37:44 +0530

    Add files via upload

---
+++ Hello-World/hello_world_chandu6111.sql
@@ -0,0 +1,14 @@
+'''
+LANGUAGE: MySQL
+AUTHOR: chandu6111
+GITHUB: https://github.com/chandu6111
+'''
+
+mysql>select "Hello world" as message;
+
+Output:
++-------------+
+| message     |
++-------------+
+| Hello world |
++-------------+
